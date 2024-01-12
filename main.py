2c9c1fc84dd920f8aff42483fcafb28b9abe4d81
sadik312 <sadikislam46@gmail.com>
2024-01-12 18:39:10 +0000

    scraped all countries from sample webste using for loop

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ page = urlopen("https://weimergeeks.com/examples/scraping/example1.html")
 soup = BeautifulSoup(page, "html.parser")
 heading = soup.h1
 print(heading.text)
+city_list = soup.find_all("td", class_="city")
+for city in city_list:
+    print(city.get_text())
