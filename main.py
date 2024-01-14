f51254c951cb5d5404016b3232fadae1a0046189
sadik312 <sadikislam46@gmail.com>
2024-01-14 13:05:48 +0000

    added website to scrape and and created GET request

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ import requests
 import openpyxl
 
 # Scrape Amazon website
+def web_scrape(search_query):
+    url=f"https://www.amazon.co.uk/"
+    headers={
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+             }
 
 # HTTP GET request to specified URL
+response = requests.get(url, headers=headers)
 
 # Check if request was successful
 
